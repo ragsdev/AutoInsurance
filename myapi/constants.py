c65823f14f8636a204c79b438e9f4c8766325eae
tharshi2803 <67555968+tharshi2803@users.noreply.github.com>
2021-08-01 00:03:15 +0530

    changed constants.py

--- myapi/constants.py
+++ myapi/constants.py
@@ -1,4 +1,4 @@
 CANNOT_SUBMIT_ON_SUNDAY = "Cannot submit a claim on Sunday"
 INVALID_CLAIM_AMOUNT = "Invalid claim amount"
 AGE_SHOULD_ABOVE_EIGHTEEN = "Age should be greater than 18"
-CANNOT_SUBMIT_FOR_FUTURE = "Cannot submit a claim for future date"
+CANNOT_SUBMIT_FOR_FUTURE_DATE= "Cannot submit a claim for future date"
