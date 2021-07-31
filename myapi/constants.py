826d1db632080704a0dbeac6dc830198be1c91ed
THiruthanikan.K <thiruthanikan.k@techmango.net>
2021-08-01 00:20:05 +0530

    Changes constants.py

--- myapi/constants.py
+++ myapi/constants.py
@@ -1,4 +1,4 @@
-CANNOT_SUBMIT_ON_SUNDAY = "Cannot submit a claim on Sunday"
+CANNOT_SUBMIT_ON_SUNDAYS = "Cannot submit a claim on Sunday"
 INVALID_CLAIM_AMOUNT = "Invalid claim amount"
 AGE_SHOULD_BE_ABOVE_EIGHTEEN = "Age should be greater than 18"
 CANNOT_SUBMIT_FOR_FUTURE_DATE= "Cannot submit a claim for future date"
