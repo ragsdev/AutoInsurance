3065286f0090b88c164f3661b18b8dc13261c7a1
tharshi2803 <67555968+tharshi2803@users.noreply.github.com>
2021-08-01 03:17:25 +0530

    chnaged views.py

--- myapi/views.py
+++ myapi/views.py
@@ -6,8 +6,34 @@ from .models import Claims
 from .serializers import ClaimsSerializer
 from rest_framework.decorators import api_view
 from datetime import datetime, date
-#import calendar
+# import calendar
 from . import constants
+
+
+def check_age(birth_date):
+    todays_date = date.today()
+    birth_date_obj = datetime.strptime(birth_date, '%Y-%m-%d')
+    current_age = (todays_date.year - birth_date_obj.year)
+    if current_age < 18:
+        return True
+    else:
+        return False
+
+
+def get_current_age(birth_date):
+    todays_date = date.today()
+    birth_date_obj = datetime.strptime(birth_date, '%Y-%m-%d')
+    current_age = (todays_date.year - birth_date_obj.year)
+    return current_age
+
+
+def check_if_sunday(p_date):
+    created_date_obj = datetime.strptime(p_date, '%Y-%m-%d')
+    currweekday = created_date_obj.weekday()
+    if currweekday == 6:
+        return True
+    else:
+        return False
 
 
 @api_view(['POST'])
@@ -15,64 +41,47 @@ def submit_claims(request):
     if request.method == 'POST':
         claims_data = JSONParser().parse(request)
 
-        if((int(claims_data['claims_amount'])< 1000) or (int(claims_data['claims_amount']) > 50000)):
+        if (int(claims_data['claims_amount']) < 1000) or (int(claims_data['claims_amount']) > 50000):
             print(claims_data['claims_amount'])
-            #return JsonResponse({'message': "Invalid claim amount"}, status=status.HTTP_404_NOT_FOUND)
-            return JsonResponse({'message': constants.INVALID_CLAIM_AMOUNT}, status=status.HTTP_404_NOT_FOUND)
+            # return JsonResponse({'message': "Invalid claim amount"}, status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({'message': constants.INVALID_CLAIM_AMOUNTS}, status=status.HTTP_404_NOT_FOUND)
 
-
-        #birth_date_str = claims_data['birth_date']
-
-        #birth_date_obj = datetime.strptime(birth_date_str, '%d/%m/%Y')
-        todays_date = date.today()
-        #current_age=(todays_date.year - birth_date_obj.year)
-
-        def check_age(birth_date):
-            birth_date_obj = datetime.strptime(birth_date, '%d/%m/%Y')
-            current_age = (todays_date.year - birth_date_obj.year)
-            if current_age < 18:
-                return True
-            else:
-                return False
-
+        # birth_date_str = claims_data['birth_date']
+        # birth_date_obj = datetime.strptime(birth_date_str, '%d/%m/%Y')
+        # current_age=(todays_date.year - birth_date_obj.year)
 
         birth_date_str = claims_data['birth_date']
+        current_age = get_current_age(birth_date_str)
+
         print(check_age(birth_date_str))
-        if (check_age(birth_date_str) < 18) == True:
-            #print(claims_data['birth_date'])
-            #return JsonResponse({'message': "Age should be greater than 18"}, status=status.HTTP_404_NOT_FOUND)
-            return JsonResponse({'message': constants.AGE_SHOULD_ABOVE_EIGHTEEN}, status=status.HTTP_404_NOT_FOUND)
-
-
-
-        def check_if_sunday(p_date):
-            created_date_obj = datetime.strptime(p_date, '%d/%m/%Y')
-            currweekday = created_date_obj.weekday()
-            if currweekday == 6:
-                return True
-            else:
-                return False
+        if check_age(birth_date_str):
+            # print(claims_data['birth_date'])
+            # return JsonResponse({'message': "Age should be greater than 18"}, status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({'message': constants.AGE_SHOULD_BE_ABOVE_EIGHTEEN}, status=status.HTTP_404_NOT_FOUND)
 
         created_date_str = claims_data['created_date']
-        if check_if_sunday(created_date_str) == True:
-            return JsonResponse({'message': constants.CANNOT_SUBMIT_ON_SUNDAY}, status=status.HTTP_404_NOT_FOUND)
+        if check_if_sunday(created_date_str):
+            return JsonResponse({'message': constants.CANNOT_SUBMIT_ON_SUNDAYS}, status=status.HTTP_404_NOT_FOUND)
 
-        #new_date_str = claims_data['created_date']
-        #new_date_obj = datetime.strptime(new_date_str, '%Y-%m-%d')
+        new_date_str = claims_data['created_date']
+        new_date_obj = datetime.strptime(new_date_str, '%Y-%m-%d')
 
-        #current_date = new_date_obj.weekday()
-        #if (current_date == 6):
+        # current_date = new_date_obj.weekday()
+        # if (current_date == 6):
         #    print(current_date)
-           #return JsonResponse({'message': "cannot submit on Sunday"}, status=status.HTTP_404_NOT_FOUND)
+        # return JsonResponse({'message': "cannot submit on Sunday"}, status=status.HTTP_404_NOT_FOUND)
         #   return JsonResponse({'message': constants.CANNOT_SUBMIT_ON_SUNDAY}, status=status.HTTP_404_NOT_FOUND)
+        # current_date = today_date_obj.weekday()
 
-
-        #current_date = today_date_obj.weekday()
         if new_date_obj.date() > datetime.date(datetime.now()):
-            #return JsonResponse({'message': "Cannot submit a claim for future date"}, status=status.HTTP_404_NOT_FOUND)
-            return JsonResponse({'message': constants.CANNOT_SUBMIT_FOR_FUTURE}, status=status.HTTP_404_NOT_FOUND)
+            # return JsonResponse({'message': "Cannot submit a claim for future date"},
+            # status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({'message': constants.CANNOT_SUBMIT_FOR_FUTURE_DATE}, status=status.HTTP_404_NOT_FOUND)
 
-        model_data = dict(name=claims_data['name'], age=current_age, address=claims_data['address'], license_num=claims_data['license_num'], id_proof=claims_data['id_proof'], claims_amount=claims_data['claims_amount'], created_date=claims_data['created_date'])
+        model_data = dict(name=claims_data['name'], age=current_age, address=claims_data['address'],
+                          license_num=claims_data['license_num'], id_proof=claims_data['id_proof'],
+                          claims_amount=claims_data['claims_amount'], created_date=claims_data['created_date'],
+                          vehicle_type=claims_data['vehicle_type'])
         claims_serializer = ClaimsSerializer(data=model_data)
 
         if claims_serializer.is_valid():
@@ -105,8 +114,8 @@ def claims_by_id(request, cid):
             claims = Claims.objects.get(id=cid)
         except Claims.DoesNotExist:
             error_message = "The claim no {scid} does not exist".format(scid=cid)
-            return JsonResponse({'message':  error_message}, status=status.HTTP_404_NOT_FOUND)
-        
+            return JsonResponse({'message': error_message}, status=status.HTTP_404_NOT_FOUND)
+
         claims_data = JSONParser().parse(request)
         claims_serializer = ClaimsSerializer(claims, data=claims_data)
         if claims_serializer.is_valid():
@@ -118,3 +127,4 @@ def claims_by_id(request, cid):
         claims.delete()
         error_message = "The claim no {scid} was deleted successfully".format(scid=cid)
         return JsonResponse({'message': error_message}, status=status.HTTP_204_NO_CONTENT)
+
